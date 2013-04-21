3a1b433a563693c6b01d485b50e52b4549262f85
Mark Ni <xixian.ni@gmail.com>
2013-04-21 13:01:01 -0700

    added config file

--- app.js
+++ app.js
@@ -4,29 +4,29 @@
  */
 
 var express = require('express'),
-  routes = require('./routes'),
-  api = require('./routes/api'),
-  env = process.env.NODE_ENV || 'dev';
+	routes = require('./routes'),
+	api = require('./routes/api'),
+	config = require('./config')
 
 var app = module.exports = express();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(__dirname + '/public'));
-  app.use(app.router);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.static(__dirname + '/public'));
+	app.use(app.router);
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 // Routes
@@ -45,14 +45,7 @@ app.get('*', routes.index);
 
 // Start server
 
-if (env === 'dev'){
-	app.listen(3000, function(){
-		console.log("Server started, port 3000");
-	});
-}
-else
-{
-	app.listen(8001,"192.81.131.191", function(){
-		console.log("Server started, port 80");
-	});
-}
+app.listen(config.port,config.host,function(){
+	console.log("Server started, port "+ config.port);
+});
+
